03b189ba3b98a07ec7290300dc6cf3fa2c0514e0
panaminer08 <adamsrey@outlook.com>
2018-09-06 09:58:36 -0400

    pushing all my changes to be graded

---
+++ no_name.rb
@@ -0,0 +1,32 @@
+# def pop(num)
+#     return num if num.size <= 1
+# swapped = true
+#     while swapped do 
+#         swapped = false
+#         0.upto(num.size-2) do |i|
+#             if num[i] > num[i = 1]
+#                 num[i], num[i = 1] = num[i = 1], num[i]
+#                 swapped true
+#             end
+#         end
+#     end
+
+#   num
+# end
+# pop([-3, -12, 2])
+
+# def no_name(num)
+#     l = x.length
+  
+#     for i in 0..l
+#       num[i] = (num[i] * 1) * num[i]
+#     end
+  
+#     x
+#   end
+  
+#   no_name([-3, -12, 2]) # [9, 144, 4]
+  
+print "what is your name"
+user_input = gets.chomp
+puts "your string in #{reynaldo)"
